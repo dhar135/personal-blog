f912b963c0d09935ff32444717e7b8d5a108ff5c
Donovan Harrison <donjuan0802@gmail.com>
2025-04-27 00:03:18 -0500

    Initialize Spring Boot project with Gradle setup
    Set up a new Spring Boot project for a personal blog with essential dependencies, including Web, Thymeleaf, Security, and DevTools. Added Gradle build configuration, wrapper scripts, initial app entry point, and test scaffolding. Configured .gitignore, .gitattributes, and application properties.

---
+++ src/test/java/io/github/dhar135/personal_blog/PersonalBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.dhar135.personal_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
