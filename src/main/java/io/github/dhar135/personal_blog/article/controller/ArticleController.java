b22e76d4edc8953fcb383a906b93af99b4448abd
Donovan Harrison <donjuan0802@gmail.com>
2025-05-19 19:12:29 -0500

    feat: add home page endpoint to ArticleController

--- src/main/java/io/github/dhar135/personal_blog/article/controller/ArticleController.java
+++ src/main/java/io/github/dhar135/personal_blog/article/controller/ArticleController.java
@@ -1,7 +1,12 @@
 package io.github.dhar135.personal_blog.article.controller;
 
+import io.github.dhar135.personal_blog.article.model.Article;
 import io.github.dhar135.personal_blog.article.service.ArticleService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class ArticleController {
@@ -10,6 +15,14 @@ public class ArticleController {
 
     public ArticleController(ArticleService articleService) {
         this.articleService = articleService;
+    }
+
+    // Home Page
+    @GetMapping("/home")
+    public String showHomePage(Model model) {
+        List<Article> articles = articleService.findAll();
+        model.addAttribute("articles", articles);
+        return "home";
     }
 
 }
